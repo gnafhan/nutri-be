ce41741ab9af17a6ac10840eafb270d8cb8ba86b
TheValeHack <email.penting.janngan.kepo@gmail.com>
2025-05-11 19:11:31 +0700

    fix: fix db connect

---
+++ src/database/connect.go
@@ -0,0 +1,48 @@
+package database
+
+import (
+	"app/src/config"
+	"app/src/utils"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(dbHost, dbName string) *gorm.DB {
+	// hihihi maap
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+		dbHost, config.DBUser, config.DBPassword, dbName, config.DBPort,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:                 logger.Default.LogMode(logger.Info),
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+		TranslateError:         true,
+	})
+	if err != nil {
+		utils.Log.Errorf("Failed to connect to database: %+v", err)
+	}
+
+	if err := db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";").Error; err != nil {
+		utils.Log.Errorf("Failed to enable uuid-ossp extension: %+v", err)
+	}
+
+	MigrateAndSeed(db)
+
+	sqlDB, errDB := db.DB()
+	if errDB != nil {
+		utils.Log.Errorf("Failed to connect to database: %+v", errDB)
+	}
+
+	// Config connection pooling
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(60 * time.Minute)
+
+	return db
+}
